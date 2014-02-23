ecd0e20b5344169fb1e0dff84be6e38857b4ec6a
Joe Philip Ninan <joe@porteus.example.net>
2014-02-23 15:38:17 +0530

    CL interface to AGshift calculator

--- CodesOnServer/AutoguiderShiftCalculator.py
+++ CodesOnServer/AutoguiderShiftCalculator.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 #This script is to calculate the shift user has to give
 # in order to shift the guiding source in the TIRSPEC frame.
-import math
-
-def RotateScaleVector(V,S=(1,1),rot=0):
+import math     # NOT USED while running
+import sys
+def RotateScaleVector(V,S=(1,1),rot=0):   # NOT USED while running
     """ Rotated the input 1d vector V. Input V is a python 2 element tuple, V=(X,Y). 
     Input S is the scaling vector to be applied to input 1d vector before rotation.
     Input Rot is the rotation angle in radians  
@@ -24,15 +24,28 @@ def LinearTransform(V,T=(-0.64472891,-0.69205482,0.66728573,-0.65989201)):
     return xout,yout
 
 
-cur_X, cur_Y = 519,556   #Current TIRSPEC star's X,Y coordinate
-target_X, target_Y = 461.1,555   #Targeted TIRSPEC star X,Y coordinate
+while True :
+    try:
+        CurrentTxy=raw_input("Present X Y coordinate in TIRSPEC (Eg: 450 556) : ").strip(' ') #strips away leading and trailing whitespaces.
+        TargetTxy=raw_input("Targeted X Y coordinate in TIRSPEC (Eg: 490 556) : ").strip(' ')
+        CurrentAGxy=raw_input("Present X Y coordinate in AUTOGUIDER (Eg: 1799.25 234.35) : ").strip(' ')
+    except (KeyboardInterrupt, EOFError):
+        print("\n Exiting.. \n ")
+        sys.exit(0)
 
-curAG_X, curAG_Y = 1751.11,282.83  #Current Auto Guider star's X,Y coordinate 
+    try :
+        cur_X, cur_Y = tuple(float(i) for i in CurrentTxy.split())   #Current TIRSPEC star's X,Y coordinate
+        target_X, target_Y = tuple(float(i) for i in TargetTxy.split())    #Targeted TIRSPEC star X,Y coordinate
 
-V=(target_X-cur_X,target_Y-cur_Y)   # Required displacement vector in TIRSPEC frame
-V_AG=LinearTransform(V)  # Calculated displacement vector in Auto guider frame
+        curAG_X, curAG_Y = tuple(float(i) for i in CurrentAGxy.split())   #Current Auto Guider star's X,Y coordinate 
+    except (ValueError):
+        print("Error: Wrong input given. \n---------------------------------------------")
+        continue
 
-targetAG_X=curAG_X + V_AG[0]  #Targeted Auto Guider star's X coordinate 
-targetAG_Y=curAG_Y + V_AG[1]  #Targeted Auto Guider star's Y coordinate 
+    V=(target_X-cur_X,target_Y-cur_Y)   # Required displacement vector in TIRSPEC frame
+    V_AG=LinearTransform(V)  # Calculated displacement vector in Auto guider frame
 
-print("Move AutoGuider star to X={0}, Y={1}".format(targetAG_X,targetAG_Y))
+    targetAG_X=curAG_X + V_AG[0]  #Targeted Auto Guider star's X coordinate 
+    targetAG_Y=curAG_Y + V_AG[1]  #Targeted Auto Guider star's Y coordinate 
+
+    print("***** Move AutoGuider star to X={0}, Y={1}".format(targetAG_X,targetAG_Y))
